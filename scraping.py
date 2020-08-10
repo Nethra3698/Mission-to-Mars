b45ae507ddb8c2844557ce0dff34c28258c17523
Nethra3698 <nethra.rajavel@gmail.com>
2020-08-10 00:57:16 -0400

    initial attempt at Challenge

--- scraping.py
+++ scraping.py
@@ -8,13 +8,16 @@ def scrape_all():
     # Initiate headless driver for deployment
     browser = Browser("chrome", executable_path="chromedriver", headless=True)
     news_title, news_paragraph = mars_news(browser)
+    mars_images,hemi_title = mars_hemisphere(browser)
     # Run all scraping functions and store results in dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "mars_hemisphere_images": mars_images,
+        "mars_hemisphere_titles": hemi_title
     }
     # Stop webdriver and return data
     browser.quit()
@@ -73,7 +76,7 @@ def featured_image(browser):
 
     try:
         # Find the relative image url
-        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+        img_url_rel = img_soup.select('figure.lede a img').get("src")
 
     except AttributeError:
         return None
@@ -93,6 +96,41 @@ def mars_facts():
     df.set_index('description', inplace=True)
 
     return df.to_html(classes="table table-striped") 
+
+# Challenge 
+# Scraping mars hemisphere images
+
+def mars_hemisphere(browser):
+
+    img_url=[]
+    hemi_title=[]
+
+    for x in range(0,4):
+
+        # Visit URL
+        url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        browser.visit(url)
+
+        # Find and click the full image button
+        thumbnail_elem = browser.find_by_tag('h3')[x]
+        thumbnail_elem.click()
+
+        # Find the more info button and click that
+                
+        hemi_elem = browser.find_by_id('wide-image-toggle')
+        hemi_elem.click()
+        
+        html = browser.html
+        img_soup = soup(html, 'html.parser')
+        title_name = img_soup.find('h2', {'class':'title'}).text
+
+        img_url_r = img_soup.find('img', {'class':'wide-image'}).get("src")
+
+        # Use the base URL to create an absolute URL
+        img_url.append(f'https://astrogeology.usgs.gov/{img_url_r}')
+        hemi_title.append(title_name)
+
+    return img_url, hemi_title
 
 if __name__ == "__main__":
     # If running as script, print scraped data
